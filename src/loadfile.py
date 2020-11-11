07babed3cf4951d716d742c63d63797586f5fae2
Nasim Anousheh <nasimanousheh@gmail.com>
2020-11-11 15:03:18 -0500

    rename lammps to file

--- src/loadfile.py
+++ src/loadfile.py
@@ -9,11 +9,10 @@ import MDAnalysis as mda
 from MDAnalysis.analysis import contacts
 from MDAnalysis.tests.datafiles import PSF,DCD
 
-def load_lammps(fname, debug=False):
-    lammps_file_export = open(fname, 'r')
-    lines = lammps_file_export.readlines()
+def load_files(fname, debug=False):
+    load_file_export = open(fname, 'r')
+    lines = load_file_export.readlines()
     no_lines = len(lines)
-    lammps_dix = {'index': {}}
     frames_cnt = 0
     format_data= None
     no_bonds = 0
@@ -26,8 +25,8 @@ def load_lammps(fname, debug=False):
         i=i+1
 
     frames_cnt = 0
-    lammps_file = MDAnalysis.Universe(fname,format=format_data)
-    if format_data== None:
-        bonds = lammps_file.bonds.to_indices()
-        no_bonds = len(lammps_file.bonds)
-    return lammps_file,no_bonds
+    load_file = MDAnalysis.Universe(fname,format=format_data)
+    if format_data == None:
+        bonds = load_file.bonds.to_indices()
+        no_bonds = len(load_file.bonds)
+    return load_file,no_bonds
