fa54938e06951b5b9f0948783e4763cbb96e460e
fyffep <pgf01@rocketmail.com>
2021-01-07 21:33:04 -0500

    Added the hydrogen at the caps

--- furiousatoms/nanostructure_builder.py
+++ furiousatoms/nanostructure_builder.py
@@ -21,14 +21,17 @@ from fractions import gcd
 from itertools import product
 from furiousatoms.geomlib import Atom, Molecule, Crystal, getfragments
 from furiousatoms.sharedmem import SharedMemory
+from furiousatoms.forms.widget_SWNT import Ui_Form_SWNT
+import math
 import sys
 import io
 
 thre = 1e-10
 vacuum = 4
+SM = SharedMemory()
 
 
-def nanotube_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), centered=False):
+def SWNT_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), centered=False):
     SM = SharedMemory()
     d = gcd(n, m)
     dR = 3*d if (n-m) % (3*d) == 0 else d
@@ -50,37 +53,116 @@ def nanotube_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), center
             if all(-thre < pt.dot(v) < 1-thre for v in [Ch_proj, T_proj]):
                 for k in range(N):
                     pts.append((sp, pt+k*T))
-    SM.diameter_tube = norm(Ch)/np.pi
+    diameter = norm(Ch)/np.pi
     def gr2tube(v):
         phi = 2*np.pi*v.dot(Ch_proj)
-        return np.array((SM.diameter_tube/2*np.cos(phi),
-                         SM.diameter_tube/2*np.sin(phi),
+        return np.array((diameter/2*np.cos(phi),
+                         diameter/2*np.sin(phi),
                          v.dot(T_proj)*norm(T)))
     xyz = [gr2tube(v) for _, v in pts]
-    atom_types = [v for v, _ in pts]
+    atom_types_swnt = [v for v, _ in pts]
     m = Molecule([Atom(sp, r) for (sp, _), r in zip(pts, xyz)])
     fragments = m.to_json(scale =1)
-    n_atoms = len(xyz)
-    SM.pos = np.array(xyz)
-    assert SM.pos.shape == (n_atoms, 3)
-    u = MDAnalysis.Universe.empty(n_atoms, trajectory=True, n_residues=10)
-    u.atoms.positions = SM.pos
-    SM.bonds = np.array(fragments['bonds'])
-    u.add_bonds(SM.bonds)
-    lx = 0
-    ly = 0
-    lz = 0
-    u.trajectory.ts.dimensions[0] = lx
-    u.trajectory.ts.dimensions[1] = ly
-    u.trajectory.ts.dimensions[2] = lz
-    u.add_TopologyAttr('name', atom_types)
-    u.atoms.write('C:/Users/nasim/Devel/furious-atoms/nanotube_structure.pdb')
-    if centered:
-        m = m.shifted(np.array((-1, -1, 0))*SM.diameter_tube*(vacuum+1)/2)
-    return Crystal([((vacuum+1)*SM.diameter_tube, 0, 0),
-                    (0, (vacuum+1)*SM.diameter_tube, 0),
-                    (0, 0, N*norm(T))],
-                   m.atoms)
+    lx = diameter + 1
+    ly = diameter + 1
+    lz = 30+diameter/2
+    n_atoms_swnt = len(xyz)
+    coord_array_swnt = np.array(xyz) #+ np.array((0, 0, int(lz/2)))
+    assert coord_array_swnt.shape == (n_atoms_swnt, 3)
+    swnt = MDAnalysis.Universe.empty(n_atoms_swnt, trajectory=True, n_residues=10)
+    swnt.atoms.positions = coord_array_swnt
+    all_bonds_swnt = np.array(fragments['bonds'])
+    swnt.trajectory.ts.dimensions[0] = lx
+    swnt.trajectory.ts.dimensions[1] = ly
+    swnt.trajectory.ts.dimensions[2] = lz
+    swnt.add_TopologyAttr('name', atom_types_swnt)
+    swnt.add_bonds(all_bonds_swnt)
+    swnt.atoms.write('C:/Users/nasim/Devel/furious-atoms/nanotube_structure.pdb')
+#########################################################
+    n_hydrogen = 0
+    H_coordinaes = []
+    for a in range(n_atoms_swnt):
+        indices_of_a = np.where(all_bonds_swnt == a)[0]
+
+        if len(indices_of_a) == 1:
+            end_atom_indices = (all_bonds_swnt[indices_of_a])
+            if end_atom_indices[0][0] == a:
+                end_atom_indices = (all_bonds_swnt[indices_of_a]) #[2735 2447] main= 2735
+                f_connec_to_end_atom_index = end_atom_indices[0][1] # 2447
+                core_connections = all_bonds_swnt[np.where(all_bonds_swnt == f_connec_to_end_atom_index)[0]]
+                Both_connected_atoms_with_a = np.setdiff1d(core_connections, [f_connec_to_end_atom_index])
+                Both_connected_atoms = np.setdiff1d(Both_connected_atoms_with_a, a)
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+            if end_atom_indices[0][1] == a:
+                end_atom_indices = (all_bonds_swnt[indices_of_a])
+                f_connec_to_end_atom_index = end_atom_indices[0][0]
+                core_connections = all_bonds_swnt[np.where(all_bonds_swnt == f_connec_to_end_atom_index)[0]]
+                Both_connected_atoms_with_a = np.setdiff1d(core_connections, [f_connec_to_end_atom_index])
+                Both_connected_atoms = np.setdiff1d(Both_connected_atoms_with_a, a)
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+            H_coord_1 = xyz[a] - xyz[right_connection] + xyz[f_connec_to_end_atom_index]
+            H_coord_2 = xyz[a] - xyz[left_connection] + xyz[f_connec_to_end_atom_index]
+            H_coordinaes.extend([H_coord_1])
+            H_coordinaes.extend([H_coord_2])
+            n_hydrogen = n_hydrogen + 2
+
+        if len(indices_of_a) == 2:
+            end_atom_indices = (all_bonds_swnt[indices_of_a])
+            if end_atom_indices[0][0] == a:
+                end_atom_index = end_atom_indices[0][0]
+                f_connec_to_end_atom_index = np.where(all_bonds_swnt == end_atom_index)[0]
+                core_connections = all_bonds_swnt[f_connec_to_end_atom_index]
+                Both_connected_atoms = np.setdiff1d(core_connections, [end_atom_index])
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+                first_vector = xyz[end_atom_index] - xyz[right_connection]
+                second_vector = xyz[end_atom_index] - xyz[left_connection]
+                H_coord = (first_vector + second_vector) + xyz[end_atom_index]
+                H_coordinaes.extend([H_coord])
+                n_hydrogen = n_hydrogen + 1
+
+            if end_atom_indices[0][1] == a:
+                end_atom_index = end_atom_indices[0][1]
+                f_connec_to_end_atom_index = np.where(all_bonds_swnt == end_atom_index)[0]
+                core_connections = all_bonds_swnt[f_connec_to_end_atom_index]
+                Both_connected_atoms = np.setdiff1d(core_connections, [end_atom_index])
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+
+                first_vector = xyz[end_atom_index] - xyz[right_connection]
+                second_vector = xyz[end_atom_index] - xyz[left_connection]
+                H_coord = (first_vector + second_vector) + xyz[end_atom_index]
+                H_coordinaes.extend([H_coord])
+                n_hydrogen = n_hydrogen + 1
+
+
+    if n_hydrogen > 0:
+        h = MDAnalysis.Universe.empty(n_hydrogen, trajectory=True, n_residues=10)
+        coord_array_H_indice = np.array(H_coordinaes)
+        assert coord_array_H_indice.shape == (n_hydrogen, 3)
+        h.atoms.positions = coord_array_H_indice
+        h.add_TopologyAttr('name', ['H']*n_hydrogen)
+    combined = MDAnalysis.Merge(swnt.atoms, h.atoms)
+    combined.add_bonds(all_bonds_swnt)
+    n_hydrogen = 0
+    for x in range(n_atoms_swnt):
+        indices_of_a = np.where(all_bonds_swnt == x)
+        if len(indices_of_a[0]) == 1:
+            combined.add_bonds([(x, n_atoms_swnt + n_hydrogen)])
+            combined.add_bonds([(x, n_atoms_swnt + n_hydrogen + 1)])
+            n_hydrogen = n_hydrogen + 2
+        if len(indices_of_a[0]) == 2:
+            combined.add_bonds([(x, n_atoms_swnt + n_hydrogen)])
+            n_hydrogen = n_hydrogen + 1
+###########################################
+    combined.trajectory.ts.dimensions[0] = lx
+    combined.trajectory.ts.dimensions[1] = ly
+    combined.trajectory.ts.dimensions[2] = lz
+    print(SM.H_termination_SWNT)
+    # if SM.H_termination_SWNT == 'Both ends':
+    combined.atoms.write('C:/Users/nasim/Devel/furious-atoms/nanotube_structure.pdb')
 
 def MWNT_nanotube_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), centered=False):
     SM = SharedMemory()
@@ -129,15 +211,9 @@ def MWNT_nanotube_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), c
     u.trajectory.ts.dimensions[2] = lz
     u.add_TopologyAttr('name', atom_types)
     u.atoms.write('C:/Users/nasim/Devel/furious-atoms/MWNT_nanotube_structure.pdb')
-    if centered:
-        m = m.shifted(np.array((-1, -1, 0))*SM.diameter_tube*(vacuum+1)/2)
-    return Crystal([((vacuum+1)*SM.diameter_tube, 0, 0),
-                    (0, (vacuum+1)*SM.diameter_tube, 0),
-                    (0, 0, N*norm(T))],
-                   m.atoms)
 
 def graphene_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), centered=False):
-    SM = SharedMemory()
+    # SM = SharedMemory()
     d = gcd(n, m)
     dR = 3*d if (n-m) % (3*d) == 0 else d
     t1 = (2*m+n)//dR
@@ -149,31 +225,111 @@ def graphene_builder(n, m, N=1, length=None, a=1.421, species=('C', 'C'), center
     if length:
         N = int(np.ceil(length/norm(T)))
     Ch_proj, T_proj = [v/norm(v)**2 for v in [Ch, T]]
-    basis = [np.array((0, 0,0)), (a1+a2)/3]
+    basis = [np.array((0,0,0)), (a1+a2)/3]
     pts = []
     for i1, i2 in product(range(0, n+t1+1), range(t2, m+1)):
         shift = i1*a1+i2*a2
         for sp, b in zip(species, basis):
             pt = b+shift
-            if all(-thre < pt.dot(v) < 1-thre for v in [Ch_proj, T_proj]):
-                for k in range(N):
-                    pts.append((sp, pt+k*T))
+            pts.append((sp, pt))
     xyz = [v for _, v in pts]
-    atom_types = [v for v, _ in pts]
+    atom_types_graphene = [v for v, _ in pts]
     m = Molecule([Atom(sp, r) for (sp, _), r in zip(pts, xyz)])
+    fragments = m.to_json(scale =1)
     lx = 0
     ly = 0
     lz = 0
-    fragments = m.to_json(scale =1)
-    n_atoms = len(xyz)
-    SM.pos = np.array(xyz)
-    assert SM.pos.shape == (n_atoms, 3)
-    u = MDAnalysis.Universe.empty(n_atoms, trajectory=True, n_residues=1)
-    u.trajectory.ts.dimensions[0] = lx
-    u.trajectory.ts.dimensions[1] = ly
-    u.trajectory.ts.dimensions[2] = lz
-    u.atoms.positions = SM.pos
-    SM.bonds = np.array(fragments['bonds'])
-    u.add_bonds(SM.bonds)
-    u.add_TopologyAttr('name', atom_types)
-    u.atoms.write('C:/Users/nasim/Devel/furious-atoms/graphene_structure.pdb')
+    n_atoms_graphene = len(xyz)
+    coord_array_graphene = np.array(xyz)
+    assert coord_array_graphene.shape == (n_atoms_graphene, 3)
+    graphene = MDAnalysis.Universe.empty(n_atoms_graphene, trajectory=True, n_residues=10)
+    graphene.atoms.positions = coord_array_graphene
+    all_bonds_graphene = np.array(fragments['bonds'])
+    graphene.trajectory.ts.dimensions[0] = lx
+    graphene.trajectory.ts.dimensions[1] = ly
+    graphene.trajectory.ts.dimensions[2] = lz
+    graphene.add_TopologyAttr('name', atom_types_graphene)
+    graphene.add_bonds(all_bonds_graphene)
+    graphene.atoms.write('C:/Users/nasim/Devel/furious-atoms/graphene_structure.pdb')
+#########################################################
+    n_hydrogen = 0
+    H_coordinaes = []
+    for a in range(n_atoms_graphene):
+        indices_of_a = np.where(all_bonds_graphene == a)[0]
+
+        if len(indices_of_a) == 1:
+            end_atom_indices = (all_bonds_graphene[indices_of_a])
+            if end_atom_indices[0][0] == a:
+                end_atom_indices = (all_bonds_graphene[indices_of_a]) #[2735 2447] main= 2735
+                f_connec_to_end_atom_index = end_atom_indices[0][1] # 2447
+                core_connections = all_bonds_graphene[np.where(all_bonds_graphene == f_connec_to_end_atom_index)[0]]
+                Both_connected_atoms_with_a = np.setdiff1d(core_connections, [f_connec_to_end_atom_index])
+                Both_connected_atoms = np.setdiff1d(Both_connected_atoms_with_a, a)
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+            if end_atom_indices[0][1] == a:
+                end_atom_indices = (all_bonds_graphene[indices_of_a])
+                f_connec_to_end_atom_index = end_atom_indices[0][0]
+                core_connections = all_bonds_graphene[np.where(all_bonds_graphene == f_connec_to_end_atom_index)[0]]
+                Both_connected_atoms_with_a = np.setdiff1d(core_connections, [f_connec_to_end_atom_index])
+                Both_connected_atoms = np.setdiff1d(Both_connected_atoms_with_a, a)
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+            H_coord_1 = xyz[a] - xyz[right_connection] + xyz[f_connec_to_end_atom_index]
+            H_coord_2 = xyz[a] - xyz[left_connection] + xyz[f_connec_to_end_atom_index]
+            H_coordinaes.extend([H_coord_1])
+            H_coordinaes.extend([H_coord_2])
+            n_hydrogen = n_hydrogen + 2
+
+        if len(indices_of_a) == 2:
+            end_atom_indices = (all_bonds_graphene[indices_of_a])
+            if end_atom_indices[0][0] == a:
+                end_atom_index = end_atom_indices[0][0]
+                f_connec_to_end_atom_index = np.where(all_bonds_graphene == end_atom_index)[0]
+                core_connections = all_bonds_graphene[f_connec_to_end_atom_index]
+                Both_connected_atoms = np.setdiff1d(core_connections, [end_atom_index])
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+                first_vector = xyz[end_atom_index] - xyz[right_connection]
+                second_vector = xyz[end_atom_index] - xyz[left_connection]
+                H_coord = (first_vector + second_vector) + xyz[end_atom_index]
+                H_coordinaes.extend([H_coord])
+                n_hydrogen = n_hydrogen + 1
+
+            if end_atom_indices[0][1] == a:
+                end_atom_index = end_atom_indices[0][1]
+                f_connec_to_end_atom_index = np.where(all_bonds_graphene == end_atom_index)[0]
+                core_connections = all_bonds_graphene[f_connec_to_end_atom_index]
+                Both_connected_atoms = np.setdiff1d(core_connections, [end_atom_index])
+                right_connection = Both_connected_atoms[0]
+                left_connection = Both_connected_atoms[1]
+
+                first_vector = xyz[end_atom_index] - xyz[right_connection]
+                second_vector = xyz[end_atom_index] - xyz[left_connection]
+                H_coord = (first_vector + second_vector) + xyz[end_atom_index]
+                H_coordinaes.extend([H_coord])
+                n_hydrogen = n_hydrogen + 1
+
+    if n_hydrogen > 0:
+        h = MDAnalysis.Universe.empty(n_hydrogen, trajectory=True, n_residues=10)
+        coord_array_H_indice = np.array(H_coordinaes)
+        assert coord_array_H_indice.shape == (n_hydrogen, 3)
+        h.atoms.positions = coord_array_H_indice
+        h.add_TopologyAttr('name', ['H']*n_hydrogen)
+    combined = MDAnalysis.Merge(graphene.atoms, h.atoms)
+    combined.add_bonds(all_bonds_graphene)
+    n_hydrogen = 0
+    for x in range(n_atoms_graphene):
+        indices_of_a = np.where(all_bonds_graphene == x)
+        if len(indices_of_a[0]) == 1:
+            combined.add_bonds([(x, n_atoms_graphene + n_hydrogen)])
+            combined.add_bonds([(x, n_atoms_graphene + n_hydrogen + 1)])
+            n_hydrogen = n_hydrogen + 2
+        if len(indices_of_a[0]) == 2:
+            combined.add_bonds([(x, n_atoms_graphene + n_hydrogen)])
+            n_hydrogen = n_hydrogen + 1
+###########################################
+    combined.trajectory.ts.dimensions[0] = lx
+    combined.trajectory.ts.dimensions[1] = ly
+    combined.trajectory.ts.dimensions[2] = lz
+    combined.atoms.write('C:/Users/nasim/Devel/furious-atoms/graphene_structure.pdb')
