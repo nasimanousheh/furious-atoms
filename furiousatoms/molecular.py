56580825b471f4a7d79907c9bc20b28107d6b0d3
fyffep <pgf01@rocketmail.com>
2021-06-02 17:49:19 -0400

    Deleted repeated functions

--- furiousatoms/molecular.py
+++ furiousatoms/molecular.py
@@ -4,35 +4,6 @@ from fury import utils, actor, primitive
 from furiousatoms.structure import bbox
 
 
-def delete_bonds(bond_actor, selected_bond, no_bonds):
-    vertices_bond = utils.vertices_from_actor(bond_actor)
-    no_vertices_all_bond = vertices_bond.shape[0]
-    object_indices_bonds = np.where(selected_bond is True)[0]
-    sec_bond = np.int(no_vertices_all_bond / no_bonds)
-    color_add_bond = np.array([255, 0, 0, 0], dtype='uint8')
-    vcolors_bond = utils.colors_from_actor(bond_actor, 'colors')
-    for object_index_bond in object_indices_bonds:
-        vcolors_bond[object_index_bond * sec_bond: object_index_bond * sec_bond + sec_bond] = color_add_bond
-    utils.update_actor(bond_actor)
-    bond_actor.GetMapper().GetInput().GetPointData().GetArray('colors').Modified()
-    print('The bond is deleted')
-
-
-def delete_particles(sphere_actor, no_atoms, selected):
-    vertices = utils.vertices_from_actor(sphere_actor)
-    no_vertices_all_sphere = vertices.shape[0]
-    object_indices_spheres = np.where(selected is True)[0]
-    sec = np.int(no_vertices_all_sphere / no_atoms)
-    color_add = np.array([255, 0, 0, 0], dtype='uint8')
-    vcolors = utils.colors_from_actor(sphere_actor, 'colors')
-    for object_index in object_indices_spheres:
-        vcolors[object_index * sec: object_index * sec + sec] = color_add
-    utils.update_actor(sphere_actor)
-    sphere_actor.GetMapper().GetInput().GetPointData().GetArray('colors').Modified()
-
-    print('The particle is deleted')
-
-
 class UniverseManager:
 
     def __init__(self, universe, no_bonds=0):
@@ -61,6 +32,17 @@ class UniverseManager:
         self.radii_unique_types = 0.2 + np.zeros(len(self.unique_types)) #np.random.rand(len(SM.unique_types))
         self.selected_particle = np.zeros(self.no_atoms, dtype=np.bool)
         self.selected_bond = np.zeros(self.no_bonds, dtype=np.bool)
+
+
+        # self.sphere_res = 'repulsion100'
+        # vertices, faces = primitive.prim_sphere(name=self.sphere_res,
+        #                                         gen_faces=False)
+        # res = primitive.repeat_primitive(vertices, faces, centers=self.pos, colors=colors)
+        # big_verts, big_faces, big_colors, _ = res
+        # self.sphere_actor = utils.get_actor_from_primitive(big_verts, big_faces, big_colors)
+
+
+
         self.sphere_actor = actor.sphere(centers=self.pos, colors=colors,
                                          radii=self.radii_spheres, theta=32, phi=32)
         self.all_vertices_particles = utils.vertices_from_actor(self.sphere_actor)
