3e02cbf329722634bf2434e63a1c51bfbc8529fe
Nasim Anousheh <nasimanousheh@gmail.com>
2021-10-06 15:30:14 -0400

    Remove share memory

--- furiousatoms/SWNT_builder.py
+++ furiousatoms/SWNT_builder.py
@@ -19,7 +19,6 @@ import sys
 
 thre = 1e-10
 vacuum = 4
-SM = SharedMemory()
 """
     Ui_SWNT class creates a widget for building single-wall nanotube (SWNT)
 
@@ -54,14 +53,14 @@ class Ui_SWNT(QtWidgets.QMainWindow):
         self.SWNT.pushButton_build_SWNT.clicked.connect(lambda:self.close())
 
     def SWNT_diameter_changed(self):
-        SM.H_termination_SWNT = self.SWNT.comboBox_H_termination_SWNT.currentText()
+        # H_termination_SWNT = self.SWNT.comboBox_H_termination_SWNT.currentText()
         bond_length_SWNT = self.SWNT.lineEdit_bond_length_SWNT.text()
-        SM.bond_length_SWNT = float(bond_length_SWNT)
+        bond_length_SWNT = float(bond_length_SWNT)
         value_n_SWNT = int(self.SWNT.spinBox_chirality_N_SWNT.text())
         value_m_SWNT = int(self.SWNT.spinBox_chirality_M_SWNT.text())
         repeat_units_SWNT = int(self.SWNT.spinBox_repeat_units_SWNT.text())
-        a1 = np.array((np.sqrt(3)*SM.bond_length_SWNT, 0))
-        a2 = np.array((np.sqrt(3)/2*SM.bond_length_SWNT, -3*SM.bond_length_SWNT/2))
+        a1 = np.array((np.sqrt(3)*bond_length_SWNT, 0))
+        a2 = np.array((np.sqrt(3)/2*bond_length_SWNT, -3*bond_length_SWNT/2))
         Ch = value_n_SWNT*a1+value_m_SWNT*a2
         d = gcd(value_n_SWNT, value_m_SWNT)
         dR = 3*d if (value_n_SWNT-value_m_SWNT) % (3*d) == 0 else d
@@ -76,12 +75,14 @@ class Ui_SWNT(QtWidgets.QMainWindow):
         self.SWNT.lineEdit_length_SWNT.setText(str(length_SWNT))
 
     def SWNT_builder_callback(self):
+        bond_length_SWNT = 1.421 # default value of C-C bond length
+        H_termination_SWNT = self.SWNT.comboBox_H_termination_SWNT.currentText()
         value_n_SWNT = int(self.SWNT.spinBox_chirality_N_SWNT.text())
         value_m_SWNT = int(self.SWNT.spinBox_chirality_M_SWNT.text())
         repeat_units_SWNT = int(self.SWNT.spinBox_repeat_units_SWNT.text())
         SWNT_type_1 = self.SWNT.comboBox_type1_SWNT.currentText()
         SWNT_type_2 = self.SWNT.comboBox_type2_SWNT.currentText()
-        structure_info = SWNT_builder(SM.H_termination_SWNT, value_n_SWNT, value_m_SWNT, repeat_units_SWNT, length=None, bond_length=SM.bond_length_SWNT, species=(SWNT_type_1, SWNT_type_2), centered=True)
+        structure_info = SWNT_builder(H_termination_SWNT, value_n_SWNT, value_m_SWNT, repeat_units_SWNT, length=None, bond_length=bond_length_SWNT, species=(SWNT_type_1, SWNT_type_2), centered=True)
         window = self.win.create_mdi_child()
         window.make_title()
         window.load_universe(structure_info)
@@ -117,12 +118,12 @@ def SWNT_builder(H_termination_SWNT, n, m, N=1, length=None, bond_length=1.421, 
                     pts.append((sp, pt+k*T))
 
     # Here we define the diameter of SWNT:
-    SM.diameter_SWNT = np.linalg.norm(Ch)/np.pi
+    diameter_SWNT = np.linalg.norm(Ch)/np.pi
     # This function converts the graphene to nanotube with given diameter:
     def gr2tube(v):
         phi = 2*np.pi*v.dot(Ch_proj)
-        return np.array((SM.diameter_SWNT/2*np.cos(phi),
-                         SM.diameter_SWNT/2*np.sin(phi),
+        return np.array((diameter_SWNT/2*np.cos(phi),
+                         diameter_SWNT/2*np.sin(phi),
                          v.dot(T_proj)*np.linalg.norm(T)))
     xyz = [gr2tube(v) for _, v in pts]
     atom_types_swnt = [v for v, _ in pts]
