5fb18942a9fc2efb42b8d1ed207f75ee7686f56e
Nasim Anousheh <nasimanousheh@gmail.com>
2022-02-05 21:27:08 -0500

    Added try and except for skybox sphere actor

--- furiousatoms/viewer3d.py
+++ furiousatoms/viewer3d.py
@@ -157,7 +157,11 @@ class Viewer3D(QtWidgets.QWidget):
         for act in self.universe_manager.actors():
             self.scene.add(act)
 
-        self.pbr_params_atom = sky_box_effect_atom(self.scene, self.universe_manager.sphere_actor, self.universe_manager)
+        try:
+            self.pbr_params_atom = sky_box_effect_atom(self.scene, self.universe_manager.sphere_actor, self.universe_manager)
+        except:
+            pass
+
         try:
             self.pbr_params_bond = sky_box_effect_bond(self.scene, self.universe_manager.bond_actor, self.universe_manager)
         except:
