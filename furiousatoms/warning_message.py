9b2db02bec852c988c3a56c2b5f8098b9b31a32b
fyffep <pgf01@rocketmail.com>
2022-12-11 20:30:15 -0500

    Added classes for atom and bond selection warning_messages

--- furiousatoms/warning_message.py
+++ furiousatoms/warning_message.py
@@ -85,3 +85,50 @@ class Ui_warning_charge_salt_1(QtWidgets.QMainWindow):
 
     def open_period_table(self):
         self.msg_coun.win.elect.pushButton_type_cation_salt_1.click()
+
+
+
+
+class Ui_warning_atom_delete(QtWidgets.QMainWindow):
+    """ Ui_warning_message class
+    """
+    def __init__(self, app_path=None, parent=None):
+        super(Ui_warning_atom_delete, self).__init__(parent)
+        self.msg = io.load_ui_widget("warning_message_atom_selection.ui")
+        self.v_layout = QtWidgets.QVBoxLayout()
+        self.v_layout.addWidget(self.msg)
+        self.setCentralWidget(self.msg)
+        self.setLayout(self.v_layout)
+        self.resize(657, 49)
+        self.scene = window.Scene()
+        self.showm = window.ShowManager(scene=self.scene, order_transparent=True)
+        self.init_settings()
+        self.create_connections()
+
+    def init_settings(self):
+        pass
+
+    def create_connections(self):
+        self.msg.pushButton_warning.clicked.connect(lambda:self.close())
+
+class Ui_warning_bond_delete(QtWidgets.QMainWindow):
+    """ Ui_warning_message class
+    """
+    def __init__(self, app_path=None, parent=None):
+        super(Ui_warning_bond_delete, self).__init__(parent)
+        self.msg = io.load_ui_widget("warning_message_bond_selection.ui")
+        self.v_layout = QtWidgets.QVBoxLayout()
+        self.v_layout.addWidget(self.msg)
+        self.setCentralWidget(self.msg)
+        self.setLayout(self.v_layout)
+        self.resize(657, 49)
+        self.scene = window.Scene()
+        self.showm = window.ShowManager(scene=self.scene, order_transparent=True)
+        self.init_settings()
+        self.create_connections()
+
+    def init_settings(self):
+        pass
+
+    def create_connections(self):
+        self.msg.pushButton_warning.clicked.connect(lambda:self.close())
