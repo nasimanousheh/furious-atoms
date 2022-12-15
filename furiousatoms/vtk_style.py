60027909348f2a126c2aedd858d51d912d585ab4
fyffep <pgf01@rocketmail.com>
2022-12-15 15:48:21 -0500

    Defined seperate box actor for vtk style

--- furiousatoms/vtk_style.py
+++ furiousatoms/vtk_style.py
@@ -3,7 +3,6 @@ from furiousatoms.molecular_info import get_default_molecular_info
 from furiousatoms.structure import bbox
 
 
-
 def make_aesthetic(molecule_rep):
     molecule_rep.GetProperty().SetAmbient(0.2)
     molecule_rep.GetProperty().SetDiffuse(1)
@@ -15,7 +14,8 @@ def get_vtk_ribbon(self, SM, file_name, active_vtk_window):
     molecule, all_info = get_default_molecular_info(self, SM, file_name)
     if all_info is True:
         ribbon = mol.ribbon(molecule)
-        active_vtk_window.scene.add(SM.bbox_actor)
+        SM.bbox_actor_vtk, _ = bbox(SM.box_lx, SM.box_ly, SM.box_lz, colors=SM.box_color_vtk, linewidth=2, fake_tube=True)
+        active_vtk_window.scene.add(SM.bbox_actor_vtk)
         active_vtk_window.scene.add(ribbon)
         active_vtk_window.make_title()
         active_vtk_window.render()
@@ -29,7 +29,8 @@ def get_vtk_ball_stick(self, SM, file_name, active_vtk_window):
         ball_stick_rep = mol.ball_stick(molecule, atom_scale_factor=0.3,
                                 bond_thickness=0.2)
         make_aesthetic(ball_stick_rep)
-        active_vtk_window.scene.add(SM.bbox_actor)
+        SM.bbox_actor_vtk, _ = bbox(SM.box_lx, SM.box_ly, SM.box_lz, colors=SM.box_color_vtk, linewidth=2, fake_tube=True)
+        active_vtk_window.scene.add(SM.bbox_actor_vtk)
         active_vtk_window.scene.add(ball_stick_rep)
         active_vtk_window.make_title()
         active_vtk_window.render()
@@ -42,7 +43,8 @@ def get_vtk_stick(self, SM, file_name, active_vtk_window):
     if molecule.total_num_bonds > 0:
         stick_rep = mol.stick(molecule)
         make_aesthetic(stick_rep)
-        active_vtk_window.scene.add(SM.bbox_actor)
+        SM.bbox_actor_vtk, _ = bbox(SM.box_lx, SM.box_ly, SM.box_lz, colors=SM.box_color_vtk, linewidth=2, fake_tube=True)
+        active_vtk_window.scene.add(SM.bbox_actor_vtk)
         active_vtk_window.scene.add(stick_rep)
         active_vtk_window.make_title()
         active_vtk_window.render()
@@ -56,7 +58,8 @@ def get_vtk_sphere(self, SM, file_name, active_vtk_window):
     if molecule.total_num_atoms > 0:
         sphere_cpk_rep = mol.sphere_cpk(molecule, colormode='discrete')
         make_aesthetic(sphere_cpk_rep)
-        active_vtk_window.scene.add(SM.bbox_actor)
+        SM.bbox_actor_vtk, _ = bbox(SM.box_lx, SM.box_ly, SM.box_lz, colors=SM.box_color_vtk, linewidth=2, fake_tube=True)
+        active_vtk_window.scene.add(SM.bbox_actor_vtk)
         active_vtk_window.scene.add(sphere_cpk_rep)
         active_vtk_window.render()
         active_vtk_window.show()
