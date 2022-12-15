4355bb1dd0ea51f28da1d92fe9c2381e8df8d8e7
Nasim Anousheh <nasimanousheh@gmail.com>
2022-12-15 15:47:45 -0500

    Defined seperate box actor for vtk style

--- furiousatoms/box_builder.py
+++ furiousatoms/box_builder.py
@@ -28,6 +28,8 @@ class Ui_box(QtWidgets.QMainWindow): #QWidget
     def create_connections(self):
         self.box.pushButton_build_box.clicked.connect(self.box_builder_callback)
         # self.box.SpinBox_lz.valueChanged.connect(self.initial_box_dim)
+        # self.box.SpinBox_lz.valueChanged.connect(self.initial_box_dim)
+        # self.box.SpinBox_lz.valueChanged.connect(self.initial_box_dim)
         self.box.pushButton_build_box.clicked.connect(lambda:self.close())
 
 
@@ -39,18 +41,18 @@ class Ui_box(QtWidgets.QMainWindow): #QWidget
     def box_builder_callback(self):
         active_window = self.win.active_mdi_child()
         SM = active_window.universe_manager
-        active_window.scene.rm(SM.bbox_actor)
-        box_lx = float(self.box.SpinBox_lx.text())
-        box_ly = float(self.box.SpinBox_ly.text())
-        box_lz = float(self.box.SpinBox_lz.text())
-        SM.universe.trajectory.ts.dimensions = [box_lx, box_ly, box_lz, 90, 90, 90]
+        SM.box_lx = float(self.box.SpinBox_lx.text())
+        SM.box_ly = float(self.box.SpinBox_ly.text())
+        SM.box_lz = float(self.box.SpinBox_lz.text())
+        SM.universe.trajectory.ts.dimensions = [SM.box_lx, SM.box_ly, SM.box_lz, 90, 90, 90]
         try:
             SM.box_lx = SM.universe.trajectory.ts.dimensions[0]
             SM.box_ly = SM.universe.trajectory.ts.dimensions[1]
             SM.box_lz = SM.universe.trajectory.ts.dimensions[2]
         except TypeError:
             SM.box_lx = SM.box_ly = SM.box_lz = 0
-        active_window.scene.rm(SM.bbox_actor)
+        if SM.bbox_actor:
+            active_window.scene.rm(SM.bbox_actor)
         SM.bbox_actor, _ = bbox(SM.box_lx, SM.box_ly, SM.box_lz, colors=SM.box_color, linewidth=2, fake_tube=True)
         active_window.scene.add(SM.bbox_actor)
         utils.update_actor(SM.bbox_actor)
