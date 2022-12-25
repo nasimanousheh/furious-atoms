805d294e92c5f2370a61e14330a339897bde3023
Nasim Anousheh <nasimanousheh@gmail.com>
2022-12-25 12:27:22 -0500

    Changed the path

--- furiousatoms/mainwindow.py
+++ furiousatoms/mainwindow.py
@@ -868,9 +868,9 @@ class FuriousAtomsApp(QtWidgets.QMainWindow):
             child.close()
 
     def open_dataset_fullerene(self):
-        path = 'furiousatoms/fullerene_dataset'
-        # dir_fullerene_folder = os.path.dirname(os.path.realpath(__file__))
-        dir_fullerene_folder = os.path.dirname(os.path.realpath(path))
+        # path = 'furiousatoms/fullerene_dataset'
+        dir_fullerene_folder = os.path.dirname(os.path.realpath(__file__))
+        # dir_fullerene_folder = os.path.dirname(os.path.realpath(path))
         fullerene_folder = os.path.join(dir_fullerene_folder,
                                         'fullerene_dataset')
         fname, _ = QtWidgets.QFileDialog.getOpenFileName(self, self.tr('Load'),
