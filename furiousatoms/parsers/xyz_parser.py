7fc147ed850a8b667824669e9b001013fea1aee3
Pete F <fyffep>
2023-01-30 22:22:19 -0500

    Added XYZ parser

--- furiousatoms/parsers/xyz_parser.py
+++ furiousatoms/parsers/xyz_parser.py
@@ -1,48 +1,44 @@
 import numpy as np
+from furiousatoms.parsers.base_parser import BaseParser
 from util import float_or_zero
 
-class XYZParser:
+class XYZParser(BaseParser):
     def __init__(self) -> None:
-        self.box_size = [0, 0, 0]
-        self.positions = []
-        self.bonds = []
-        self.atom_types = []
-        self.errors = "" #error messages to help user debug their file
-        self.lineId = 0 #index of line being processed
+        super().__init__()
+        self.nextWordIndex = 0
 
-    def parse(self, fname):
-        def parseAtom(line):
+    def nextWord(self):
+        for i in range(self.nextWordIndex, len(self.words)):
+            if len(self.words[i]) > 0:
+                self.nextWordIndex += 1
+                return self.words[i]
+        raise EOFError("No more words remain for the current line")
+
+
+    def parseLine(self, line):
+        #Ignore 0th & 1st lines
+        if len(line.strip()) > 0 and self.lineId >= 2:
             try:
-                pos = np.zeros((3))
-                pos = np.zeros((3))
-                # print("|"+line[8:20]+"|")
-                # print("|"+line[22:31]+"|")
-                # print("|"+line[32:42]+"|")
-                pos[0] = float_or_zero(line[8:20])
-                pos[1] = float_or_zero(line[22:31])
-                pos[2] = float_or_zero(line[32:42])
-                self.positions.append(pos)
-            except IndexError:
-                self.errors += "Line #%d is too short to read an atom's position. Please refer to a guide for LAMMPS format if you are unsure.\n"%(self.lineId)
-            except:
-                self.errors += "Failure processing line #%d.\n"%(self.lineId)
-
-
-        with open(fname, "r") as fp:
-            while True:
-                line = fp.readline()
-                if not line:
-                    break
+                #Since XYZ supports arbitrary decimal precision, 
+                #search for non-blank words only.
+                self.nextWordIndex = 0
+                self.words = line.split()
                 
-                #Try to parse regardless of whether line is a comment or a position
-                parseAtom(line)
-                self.lineId += 1
-        
-        print(self.errors) #TODO display popup
-    
-        return self.box_size, np.array(self.positions), np.array(self.bonds), np.array(self.atom_types)
+                #Element symbol or atomic number
+                self.nextWord() #skip the word
+
+                #Parse XYZ position
+                pos = np.zeros((3))
+                pos[0] = float_or_zero(self.nextWord())
+                pos[1] = float_or_zero(self.nextWord())
+                pos[2] = float_or_zero(self.nextWord())
+                self.positions.append(pos)
+            except:
+                self.errors += "Unable to parse atom on line #%d"%(self.lineId)
+
+        self.lineId += 1
 
 #TODO remove
 if __name__ == "__main__":
     parser = XYZParser()
-    parser.parse("C:\\Users\\Pete\\Desktop\\furious-atoms-exercises\\one.xyz")
+    parser.parse("C:\\Users\\Pete\\Desktop\\\Example_with_less_atoms\\graphdiyne_unitcell.xyz")
