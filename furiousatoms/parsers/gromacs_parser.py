72547b118a402f5100ba283452be560cb6b83980
Pete F <fyffep>
2023-01-30 21:31:25 -0500

    Refactored 3 parsers to inherit from abstract class

--- furiousatoms/parsers/gromacs_parser.py
+++ furiousatoms/parsers/gromacs_parser.py
@@ -1,99 +1,82 @@
 import numpy as np
+from furiousatoms.parsers.base_parser import BaseParser
 from util import float_or_zero
 
 #TODO: Refactor to use iterator pattern for the words
 
-class GROMACSParser:
+class GROMACSParser(BaseParser):
     def __init__(self) -> None:
-        self.box_size = [0, 0, 0]
-        self.positions = []
-        self.bonds = []
-        self.atom_types = []
-        self.errors = "" #error messages to help user debug their file
-        self.lineId = 0 #index of line being processed
-        
+        super().__init__()
         self.numAtoms = 2 ** 31 #default is near max int
 
-    def parse(self, fname):
-        def parseAtomCount(line):
+    def parseAtomCount(self, line):
             try:
                 self.numAtoms = int(line)
             except:
                 #Atom count is not truly necessary, but tell the user anyway.
                 self.errors += "Line #2 must contain the number of atoms as per GROMACS format."
 
-        def parseAtom(line):
-            #Since GROMACS supports arbitrary decimal precision, 
-            #search for non-blank words only.
-            wordsRead = 0
-            words = line.split()
-            pos = np.zeros((3))
-            for word in words:
-                if len(word) > 0:
-                    wordsRead += 1
-                
-                #First (0th) word is residue number
-                if wordsRead == 1: 
-                    pass
-                #Residue name
-                elif wordsRead == 2: 
-                    pass
-                #Atom name
-                elif wordsRead == 3: 
-                    pass
-                #Word #4 is X, word #5 is Y, and word #6 is Z => store these as a position.
-                elif wordsRead >= 4 and wordsRead <= 6: #Position
-                    k = wordsRead - 4
-                    pos[k] = float_or_zero(words[wordsRead - 1])
-                    if wordsRead == 6: #all 3 coords read
-                        self.positions.append(pos)
-                #Velocity (in nm/ps (or km/s), x y z in 3 columns, each 8 positions with 4 decimal places
-                elif wordsRead >= 7 and wordsRead <= 9: 
-                    pass
-                else:
-                    continue
+    def parseAtom(self, line):
+        #Since GROMACS supports arbitrary decimal precision, 
+        #search for non-blank words only.
+        wordsRead = 0
+        words = line.split()
+        pos = np.zeros((3))
+        for word in words:
+            if len(word) > 0:
+                wordsRead += 1
+            
+            #First (0th) word is residue number
+            if wordsRead == 1: 
+                pass
+            #Residue name
+            elif wordsRead == 2: 
+                pass
+            #Atom name
+            elif wordsRead == 3: 
+                pass
+            #Word #4 is X, word #5 is Y, and word #6 is Z => store these as a position.
+            elif wordsRead >= 4 and wordsRead <= 6: #Position
+                k = wordsRead - 4
+                pos[k] = float_or_zero(words[wordsRead - 1])
+                if wordsRead == 6: #all 3 coords read
+                    self.positions.append(pos)
+            #Velocity (in nm/ps (or km/s), x y z in 3 columns, each 8 positions with 4 decimal places
+            elif wordsRead >= 7 and wordsRead <= 9: 
+                pass
+            else:
+                continue
 
-        def parseBoxSize(line):
-            wordsRead = 0
-            words = line.split()
-            for word in words:
-                if len(word) > 0:
-                    wordsRead += 1
+    def parseBoxSize(self, line):
+        wordsRead = 0
+        words = line.split()
+        for word in words:
+            if len(word) > 0:
+                wordsRead += 1
 
-                #First 3 words indicate XYZ box size
-                if wordsRead >= 1 and wordsRead <= 3: 
-                    self.box_size[wordsRead - 1] = float_or_zero(word)
-            if wordsRead < 3:
-                self.errors += "At least 3 dimensions are needed for the box size on line #%d."%(self.lineId)
+            #First 3 words indicate XYZ box size
+            if wordsRead >= 1 and wordsRead <= 3: 
+                self.box_size[wordsRead - 1] = float_or_zero(word)
+        if wordsRead < 3:
+            self.errors += "At least 3 dimensions are needed for the box size on line #%d."%(self.lineId)
 
-
-        with open(fname, "r") as fp:
-            parserMethod = None
-            while True:
-                line = fp.readline()
-                if not line:
-                    break
-                
-                #Choose how to parse the current line
-                if self.lineId == 1:
-                    parserMethod = parseAtomCount
-                elif self.lineId == self.numAtoms + 2: #"final" line
-                    parserMethod = parseBoxSize
-                else:
-                    parserMethod = parseAtom
-                
-                #Perform the parse
-                if len(line.strip()) > 0 and parserMethod != None:
-                    parserMethod(line)
-                else:
-                    self.errors += "Unable to process line #%d.\n"%(self.lineId)
-                self.lineId += 1
+    def parseLine(self, line):
+        #Choose how to parse the current line
+        if self.lineId == 1:
+            self.parserMethod = self.parseAtomCount
+        elif self.lineId == self.numAtoms + 2: #"final" line
+            self.parserMethod = self.parseBoxSize
+        else:
+            self.parserMethod = self.parseAtom
         
-        print(self.errors) #TODO display popup
-    
-        return self.box_size, np.array(self.positions), np.array(self.bonds), np.array(self.atom_types)
+        #Perform the parse
+        if len(line.strip()) > 0 and self.parserMethod != None:
+            self.parserMethod(line)
+        else:
+            self.errors += "Unable to process line #%d.\n"%(self.lineId)
+        self.lineId += 1
 
 #TODO remove
 if __name__ == "__main__":
     parser = GROMACSParser()
-    parser.parse("C:\\Users\\Pete\\Desktop\\furious-atoms-exercises\\one.xyz")
+    parser.parse("C:\\Users\\Pete\\Desktop\\furious-atoms-exercises\\one.gro")
--- furiousatoms/parsers/pdb_parser.py
+++ furiousatoms/parsers/pdb_parser.py
@@ -1,60 +1,49 @@
 import numpy as np
+from furiousatoms.parsers.base_parser import BaseParser
 from util import float_or_zero
 
 #TODO maybe make error handlers less rigid?
 #TODO atom types!
 
-class PDBParser:
-    def parse(self, fname):
-        box_size = [0, 0, 0]
-        positions = []
-        bonds = []
-        atom_types = []
-        errors = "" #error messages to help user debug their file
-        with open(fname, "r") as fp:
-            lineId = 0
-            while True:
-                line = fp.readline()
-                if not line:
-                    break
+class PDBParser(BaseParser):
+    def parseLine(self, line):
 
-                if line.startswith("CONECT"): #Note the spelling. 
-                    for i, j in ((12,16), (17,21), (22,26)): #indices of atom IDs to connect to within the string
-                        try:
-                            otherId = int(line[i:j]) - 1 #ID of atom to connect to
-                            bond = np.zeros((2), dtype='int')
-                            bond[0] = abs(int(line[7:11]) - 1) #ID of first atom
-                            bond[1] = abs(otherId)
-                            if bond[0] != bond[1]:
-                                bonds.append(bond)
-                        except ValueError:
-                            pass
-                            # errors += "Refusing to connect a bond on line #%d.\n"%(lineId)
-                        except IndexError:
-                            if len(line) < 13:
-                                errors += "Line #%d is too short. At least two atom IDs are needed for a bond.\n"%(lineId)
-                elif line.startswith("ATOM"):
-                    try:
-                        pos = np.zeros((3))
-                        pos[0] = float_or_zero(line[31:38])
-                        pos[1] = float_or_zero(line[39:46])
-                        pos[2] = float_or_zero(line[47:54])
-                        positions.append(pos)
-                    except:
-                        errors += "Failure processing line #%d.\n"%(lineId)
-                elif line.startswith("REMARK"):
-                    pass
-                elif line.startswith("CRYST1 "):
-                    if len(line) >= 33:
-                        box_size[0] = float_or_zero(line[7:15])
-                        box_size[1] = float_or_zero(line[16:24])
-                        box_size[2] = float_or_zero(line[25:33])
-                    else:
-                        errors += "Line #%d is too short. Three numbers are needed for the box size.\n"%(lineId)
-                else:
-                    errors += "Unable to process line #%d.\n"%(lineId)
-                lineId += 1
+        if line.startswith("CONECT"): #Note the spelling. 
+            for i, j in ((12,16), (17,21), (22,26)): #indices of atom IDs to connect to within the string
+                try:
+                    otherId = int(line[i:j]) - 1 #ID of atom to connect to
+                    bond = np.zeros((2), dtype='int')
+                    bond[0] = abs(int(line[7:11]) - 1) #ID of first atom
+                    bond[1] = abs(otherId)
+                    if bond[0] != bond[1]:
+                        self.bonds.append(bond)
+                except ValueError:
+                    errors += "Refusing to connect a bond on line #%d.\n"%(self.lineId)
+                except IndexError:
+                    if len(line) < 13:
+                        self.errorShortLine(line)
+        elif line.startswith("ATOM"):
+            try:
+                pos = np.zeros((3))
+                pos[0] = float_or_zero(line[31:38])
+                pos[1] = float_or_zero(line[39:46])
+                pos[2] = float_or_zero(line[47:54])
+                self.positions.append(pos)
+            except:
+                self.errors += "Failure processing line #%d.\n"%(self.lineId)
+        elif line.startswith("REMARK"):
+            pass
+        elif line.startswith("CRYST1 "):
+            if len(line) >= 33:
+                self.box_size[0] = float_or_zero(line[7:15])
+                self.box_size[1] = float_or_zero(line[16:24])
+                self.box_size[2] = float_or_zero(line[25:33])
+            else:
+                self.errors += "Line #%d is too short. Three numbers are needed for the box size.\n"%(self.lineId)
+        else:
+            self.errors += "Unable to process line #%d.\n"%(self.lineId)
         
-        print(errors) #TODO display popup
-    
-        return box_size, np.array(positions), np.array(bonds), np.array(atom_types)
+#TODO remove
+if __name__ == "__main__":
+    parser = PDBParser()
+    parser.parse("C:\\Users\\Pete\\Desktop\\\Example_with_less_atoms\\graphdiyne_unitcell.pdb")
