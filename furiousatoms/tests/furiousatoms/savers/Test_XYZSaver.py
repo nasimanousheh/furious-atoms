1ffca46c49deb238e2f6c3cd1fc85df3fd7b9bdd
Pete F <fyffep>
2023-02-13 21:18:06 -0500

    Added tests for XYZSaver

---
+++ furiousatoms/tests/furiousatoms/savers/Test_XYZSaver.py
@@ -0,0 +1,37 @@
+import os
+import unittest
+import pytest
+
+from furiousatoms.savers.xyz_saver import XYZSaver, HEADER
+from furiousatoms.tests.conftest import OUT_FILE_NAME
+from saver_fixtures import *
+
+OUT_FILE_PATH = "%s.xyz"%OUT_FILE_NAME
+
+
+@pytest.fixture
+def newXYZSaver():
+    saver = XYZSaver()
+    yield saver
+    #Teardown
+    if os.path.exists(OUT_FILE_PATH):
+        os.remove(OUT_FILE_PATH)
+
+
+def test_writeAllLines(newXYZSaver, boxSize, positions, bonds, atomTypes):
+    saver = newXYZSaver
+    
+    saver.writeAllLines(OUT_FILE_PATH, boxSize, positions, bonds, atomTypes)
+
+    with open(OUT_FILE_PATH, "r") as fp:
+        assert fp.readline() == "4\n"
+        assert fp.readline() == HEADER
+        assert fp.readline() == " N 17.047001 14.099000 3.625000\n"
+        assert fp.readline() == " C 16.966999 12.784000 4.338000\n"
+        assert fp.readline() == " O 5.929000 6.358000 5.055000\n"
+        assert fp.readline() == " H 12.951000 13.245000 -2.112000\n"
+        assert fp.readline() == ''
+
+
+if __name__ == '__main__':
+    unittest.main()
