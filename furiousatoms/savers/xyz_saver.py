c7eb4ba5af1a31263fbe801749fc544f04fa88f8
fyffep <pgf01@rocketmail.com>
2023-02-13 21:18:06 -0500

    Added tests for XYZSaver

--- furiousatoms/savers/xyz_saver.py
+++ furiousatoms/savers/xyz_saver.py
@@ -1,5 +1,4 @@
 import numpy as np
-from furiousatoms.parsers.xyz_parser import XYZParser #TODO remove
 
 HEADER = "Created by FURIOUS ATOMS. By default the box dimensions are 90x90x90 cubic angstrom\n"
 
@@ -12,19 +11,3 @@ class XYZSaver():
             for i in range(len(positions)):
                 x, y, z = positions[i]
                 fp.write("%2s %8.6f %8.6f %8.6f\n"%(atomTypes[i], x, y, z))
-
-
-#TODO remove
-if __name__ == "__main__":
-    INPUT_FNAME = "C:\\Users\\Pete\\Desktop\\furious-atoms\\examples\\Graphdiyne\\graphdiyne_unitcell.xyz"
-    OUTPUT_FNAME = "C:\\Users\\Pete\\Desktop\\furious-atoms\\examples\\graphdiyne_test.xyz"
-    boxSize, positions, bonds, atomTypes = XYZParser().parse(INPUT_FNAME)
-    saver = XYZSaver()
-    saver.writeAllLines(OUTPUT_FNAME, boxSize, positions, bonds, atomTypes)
-
-    with open(OUTPUT_FNAME, 'r') as fp:
-        while True:
-            line = fp.readline()
-            if not line:
-                break
-            print(line, end='')
---
+++ furiousatoms/tests/conftest.py
@@ -0,0 +1,3 @@
+OUT_FILE_NAME = "TEST_SAVE_FILE" #saved in root directory
+
+#Use conftest to put fixtures shared amongst all tests
