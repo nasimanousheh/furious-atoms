25f6e799710e63683ab6046742c7f1df21e62c7a
fyffep <pgf01@rocketmail.com>
2023-03-04 14:48:10 -0500

    Removed MDAnalysis from graphene builder

--- furiousatoms/builders/builder_util.py
+++ furiousatoms/builders/builder_util.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def copy_bonds(bonds, num_unitcell_in_lx, num_unitcell_in_ly, UNIT_ATOM_COUNT):
+def copy_bonds(bonds, num_unitcell_in_lx, num_unitcell_in_ly, unit_atom_count):
     copied_bonds = np.zeros(shape=(len(bonds) * num_unitcell_in_lx * num_unitcell_in_ly, 2),
             dtype=int)
     bondIndex = 0
     for u in range(num_unitcell_in_lx * num_unitcell_in_ly):
         for bond in bonds:
-            offset = u * UNIT_ATOM_COUNT
+            offset = u * unit_atom_count
             for i in range(0, 2):
                 copied_bonds[bondIndex][i] = bond[i] + offset
             bondIndex += 1
