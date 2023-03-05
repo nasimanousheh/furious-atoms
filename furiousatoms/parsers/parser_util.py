2e591d095425f902ff98a7f27a9f3133a5c356b2
fyffep <pgf01@rocketmail.com>
2023-03-05 14:35:56 -0500

    Fixed view mode; added HETATM to PDB parsing

--- furiousatoms/parsers/parser_util.py
+++ furiousatoms/parsers/parser_util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def float_or_zero(val):
     try:
         return float(val)
@@ -10,3 +12,8 @@ def has_bond(bonds, new_bond):
         or bonds[i][1] == new_bond[0] and bonds[i][0] == new_bond[1]:
             return True
     return False
+
+def clean_bonds(bonds, atom_count):
+    if not bonds.shape[0] > 0 or not bonds.shape[1] == 2:
+        return np.empty(shape=(0, 2), dtype=int)
+    return bonds[((bonds[:,0] < atom_count) & (bonds[:,1] < atom_count))]
