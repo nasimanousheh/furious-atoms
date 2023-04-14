103be972869fb54d01ed972b456005e83a6a85b6
fyffep <pgf01@rocketmail.com>
2023-04-13 22:26:23 -0400

    Improved performance of bond guessing by reducing k in KNN

--- furiousatoms/bond_guesser.py
+++ furiousatoms/bond_guesser.py
@@ -1,17 +1,32 @@
-#TODO: if vdw is unknown for an element (e.g. C1), assume it is carbon and use tcarbon's vdw
-
-from furiousatoms.molecular import periodic_table #TODO move to SHM
-from math import sqrt
+from furiousatoms.molecular import periodic_table
 import numpy as np
 import kdtree
+from furiousatoms.element_lookup import lookup_num_bonds_by_atomic_number
 
 def generate_vdw_radii_map(structure):
     type_to_vdw_map = {}
-    for typ in np.unique(structure.atom_types):
-        atomic_number = periodic_table.atomic_number(typ)
+    for typ in np.unique(structure.atom_types):        
+        type_without_numbers = ''.join([i for i in typ if not i.isdigit()]) #For ions like C6 or C4, treat as C
+        atomic_number = periodic_table.atomic_number(type_without_numbers)
         vdw = periodic_table.GetVDWRadius(atomic_number)
+        #If VDW is unknown, use 0 and do not form bonds with that element.
+
         type_to_vdw_map[typ] = vdw
+
     return type_to_vdw_map
+
+def generate_max_bonds_map(structure):
+    max_bonds_map = {}
+    for typ in np.unique(structure.atom_types):        
+        type_without_numbers = ''.join([i for i in typ if not i.isdigit()]) #For ions like C6 or C4, treat as C
+        atomic_number = periodic_table.atomic_number(type_without_numbers)
+        max_num_bonds = lookup_num_bonds_by_atomic_number(atomic_number)
+        if max_num_bonds < 1:
+            max_num_bonds = 1
+
+        max_bonds_map[typ] = max_num_bonds
+
+    return max_bonds_map
 
 
 
@@ -29,96 +44,76 @@ class KDTreeItem(object):
     def __repr__(self):
         return 'Item({}, {}, {})'.format(self.coords[0], self.coords[1], self.coords[2], self.id)
 
-# def guess_bonds(structure):
-#     structure.type_to_vdw_map = structure.compute_van_der_waals_radii()
-
-#     # box_size = [
-#     #     max(structure.pos[:,0]) - min(structure.pos[:,0]), 
-#     #     max(structure.pos[:,1]) - min(structure.pos[:,1]), 
-#     #     max(structure.pos[:,2]) - min(structure.pos[:,2])
-#     # ]
-    
-#     # largest_vdw = 1.89
-#     # max_cutoff = 2.0 * largest_vdw
-#     # min_cutoff = max_cutoff if box is not None else None
-#     # min_cutoff = 1.2
-
-#     # max_cutoff = max(type_to_vdw_map.values())
-#     # min_cutoff = min(type_to_vdw_map.values())
-    
-#     #TODO try making point_list a numpy arr, then use tolist() during kdtree creation
-#     # point_list = structure.pos.tolist()
-#     atom_count = len(structure.pos)
-#     point_list = []  #[[0, 0, 0, 0] * len(structure.pos)]
-#     for i in range(atom_count):
-#         point_list.append(Item(structure.pos[i], i))
-
-#     tree = kdtree.create(point_list=point_list, dimensions=3)
-
-#     bond_list = []
-
-#     # get_pairs(tree, min_cutoff, max_cutoff)
-#     get_pairs(tree, structure, bond_list)
-#     print(bond_list)
-
-#     kdtree.visualize(tree)
-
-#     bond_arr = np.array(bond_list, dtype=int)
-#     return bond_arr
-
-
-
-
 
 
 def guess_bonds(structure):
+    '''
+    Return an array of bonds. The formula for guessing is the 
+    same as in MDAnalysis and VMD:
+    If (distance between 2 atoms) < (atom 1 VDW radius + atom 2 VDW radius)*FUDGE_FACTOR,
+    form a bond. VDW means van der Waals.
+    '''
     #For each and every node A in the tree
     #get its knn (4 nearest neighbors) B, C, D, E.
     #Check if the distance between A and B is < cutff (or sum of vdw).
 
-    MIN_CUTOFF = 1.0
-    K = 4
-    FUDGE_FACTOR = 1.0 #0.55
+    MIN_CUTOFF = 0.2
+    FUDGE_FACTOR = 0.8 #0.6
+    STRICT_MAX_BONDS = 4 #by law of chemistry, no atom can form >4 bonds
 
     type_to_vdw_map = generate_vdw_radii_map(structure)
+    max_bonds_map = generate_max_bonds_map(structure)
 
     atom_count = len(structure.pos)
-    point_list = []  #[[0, 0, 0, 0] * len(structure.pos)]
+    point_list = []
     for i in range(atom_count):
         point_list.append(KDTreeItem(structure.pos[i], i))
     
     tree = kdtree.create(point_list=point_list, dimensions=3)
     
+    #Sort the atoms by the valence so that the atoms that can form the least number
+    #of bonds (like hydrogen) are towards the front.
+    all_nodes = []
+    for node in tree.inorder():
+        all_nodes.append(node)
+    all_nodes.sort(key=lambda node: -max_bonds_map[structure.atom_types[node.data.id]])
+
+    connections_map = {} #example: {1: [2,3], 2: [1], 3: [1]}
+
     bond_list = []
-    for src_node in tree.inorder():
-        src_typ = structure.atom_types[src_node.data.id]
+    for src_node in all_nodes: #src = source
+        src_id = src_node.data.id
+        src_typ = structure.atom_types[src_id]
         src_vdw = type_to_vdw_map[src_typ]
-        for neighbor in tree.search_knn(src_node.data, K):
-            dist = neighbor[1] #distance(node.data, neighbor.data)
-            neighbor_id = neighbor[0].data.id
-            # print("Dist between",node.data,"and",neighbor[0].data,"is",dist)
-
-            if tree.left:
-                dst_typ = structure.atom_types[neighbor[0].data.id]
-                dst_vdw = type_to_vdw_map[dst_typ]
-                # print("compare ",dist,((src_vdw + dst_vdw) * 0.55))
-                if dist < (src_vdw + dst_vdw) * FUDGE_FACTOR and dist > MIN_CUTOFF:
-                    bond_list.append([src_node.data.id, neighbor_id])
-
-        # print("---")
-    
-    print(bond_list)
-
-    # kdtree.visualize(tree)
+        if src_id not in connections_map:
+            connections_map[src_id] = []
+        '''
+        k is the number of neighbors to check if there is a bond with.
+        A small k gives better performance, but it may not find every 
+        neighbor--so we add 1 to it. It may seem arbitrary, but it works.
+        '''
+        k = max_bonds_map[src_typ]
+        if k == 0:
+            continue
+        elif k < STRICT_MAX_BONDS:
+            k += 1
+        
+        for dst_node in tree.search_knn(src_node.data, k): #dst = destination (neighbor atom)
+            distance = dst_node[1]
+            dst_id = dst_node[0].data.id
+            existing_dst_bonds = connections_map.get(dst_id, [])
+            if len(existing_dst_bonds) >= STRICT_MAX_BONDS or src_id in existing_dst_bonds:
+                continue
+            
+            dst_typ = structure.atom_types[dst_id]
+            dst_vdw = type_to_vdw_map[dst_typ]
+            if distance < (src_vdw + dst_vdw) * FUDGE_FACTOR and distance > MIN_CUTOFF:
+                bond_list.append([src_id, dst_id])
+                #Record the connection to avoid making a duplicate bond
+                connections_map.get(src_id).append(dst_id)
+                if dst_id not in connections_map:
+                    connections_map[dst_id] = []
+                connections_map.get(dst_id).append(src_id)
 
     bond_arr = np.array(bond_list, dtype=int)
     return bond_arr
-
-
-if __name__ == "__main__":
-    from furiousatoms.parsers.pdb_parser import PDBParser
-    parser = PDBParser()
-    # structure = parser.parse("C:\\Users\\Pete\\Desktop\\furious-atoms-exercises\\p38-MAPK14.pdb")
-    structure = parser.parse("C:\\Users\\Pete\\Desktop\\furious-atoms-exercises\\CB_18\\CB_18.pdb")
-    
-    guessed_bonds = guess_bonds(structure)
